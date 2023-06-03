190d1fc516a4a1720458e8e7756bb2d4a12671ac
Matthew <5273302+Matthew1471@users.noreply.github.com>
2023-06-03 01:50:10 +0100

    Updated Gateway Generate Docs
    - Added the document generator now is file_depth aware for the link to readme.
    - Changed the endpoint description to be more controlled by the metadata.
    - Changed the endpoint description to be in a description dictionary within the JSON rather than 2 keys.
    - Refactored "list_items" to "new_list_items" to make it more clear which are the new list items and which are the older list items.
    - Array of primitives now writes "Array of".
    - Removed "value" from the internal JSON if the value is just the type. The output will now automatically include "type" if "value" is not present.
    - Refactored "table_to_string" to "get_table_section".
    - Refactored "schema_string" and "output" to "result" to be consistent with other functions.
    - Reference "request" and "response" parts of the endpoint JSON to reduce lookups inside "endpoint".

--- Python/examples/gateway_generate_docs.py
+++ Python/examples/gateway_generate_docs.py
@@ -26,7 +26,7 @@ from enphase_api.local.gateway import Gateway
 # Enable this mode to perform no actual requests.
 test_only = True
 
-def get_header_section(endpoint):
+def get_header_section(endpoint, file_depth=1):
     result = '= ' + endpoint['name'] + '\n'
     result += ':toc: preamble\n'
     result += 'Matthew1471 <https://github.com/matthew1471[@Matthew1471]>;\n\n'
@@ -58,13 +58,13 @@ def get_header_section(endpoint):
     result += ':url-repo: {url-org}/Enphase-API\n'
     result += ':url-contributors: {url-repo}/graphs/contributors\n\n'
 
-    result += 'A HTTP GET to `/' + endpoint['uri'] + '` can be used to get ' + endpoint['long_description'] + '.\n'
+    result += endpoint['description']['long'] + '\n'
 
     result += '\n== Introduction\n\n'
 
     result += 'Enphase-API is an unofficial project providing an API wrapper and the documentation for Enphase(R)\'s products and services.\n\n'
 
-    result += 'More details on the project are available from the link:../../../README.adoc[project\'s homepage].\n'
+    result += 'More details on the project are available from the link:' + ('../' * (file_depth + 1)) + 'README.adoc[project\'s homepage].\n'
 
     return result
 
@@ -84,7 +84,7 @@ def get_request_section(request_json):
     result += '|Values\n'
     result += '|Description\n\n'
 
-    # Table rows.
+    # Table Rows.
     for query_item in request_json:
         result += '|`' + query_item['name'] + '` ' + ('(Optional)' if 'optional' in query_item and query_item['optional'] else '') + '\n'
         result += '|' + query_item['type'] + '\n'
@@ -145,9 +145,9 @@ def get_schema(json_object, table_name='', object_map=None):
                     child_table_name = (table_name + '.' if len(table_name) else '') + json_key.capitalize()
 
                 # Take each of the items in the list and combine all the keys and their metadata.
-                list_items = {}
+                new_list_items = {}
                 for list_item in json_value:
-                    list_items.update(get_schema(list_item, table_name=child_table_name, object_map=object_map)[child_table_name])
+                    new_list_items.update(get_schema(list_item, table_name=child_table_name, object_map=object_map)[child_table_name])
 
                 # If this has been mapped/merged to a duplicate table name then we will need to append the existing dictionary.
                 if child_table_name in child_tables:
@@ -157,7 +157,7 @@ def get_schema(json_object, table_name='', object_map=None):
                     old_list_items = None
 
                 # Take each of the new list item keys.
-                for new_list_item_key, new_list_item_value in list_items.items():
+                for new_list_item_key, new_list_item_value in new_list_items.items():
                     # Is this new key not present in all of the new items or not present in the old list of item keys (if applicable)?
                     if any(new_list_item_key not in item for item in json_value) or (old_list_items and new_list_item_key not in old_list_items):
                         # Mark this new key as optional.
@@ -167,16 +167,16 @@ def get_schema(json_object, table_name='', object_map=None):
                 if old_list_items:
                     # Take all the old list item keys.
                     for old_list_item_key, old_list_item_value in old_list_items.items():
-                        # Is this old key not present in all the new items?
+                        # Is this old key not present in all of the new items?
                         if any(old_list_item_key not in item for item in json_value):
                             # Mark this old key as optional.
                             old_list_item_value['optional'] = True
 
                     # Get the existing dictionary.
-                    child_tables[child_table_name].update(list_items)
+                    child_tables[child_table_name].update(new_list_items)
                 else:
                     # Add the schema of this list of nested tables to child_tables.
-                    child_tables[child_table_name] = list_items
+                    child_tables[child_table_name] = new_list_items
 
                 # Add the type of this key.
                 current_table_fields[json_key] = {'type':'Array(Object)', 'value':'Array of `' + child_table_name + '`'} 
@@ -184,12 +184,12 @@ def get_schema(json_object, table_name='', object_map=None):
             # This is just an array of standard JSON types.
             else:
                 # Add the type of this key.
-                current_table_fields[json_key] = {'type':'Array(' + get_type_string(json_value) + ')', 'value':get_type_string(json_value)} 
+                current_table_fields[json_key] = {'type':'Array(' + get_type_string(json_value) + ')', 'value': 'Array of ' + get_type_string(json_value)} 
 
         # This is just a standard JSON type.
         else:
             # Add the type of this key.
-            current_table_fields[json_key] = {'type':get_type_string(json_value), 'value':get_type_string(json_value)}        
+            current_table_fields[json_key] = {'type':get_type_string(json_value)}        
     
     # Prepend this parent table (all its fields have been explored for nested objects).
     tables = {}
@@ -198,43 +198,45 @@ def get_schema(json_object, table_name='', object_map=None):
 
     return tables
 
-def table_to_string(table_name, table, description_map=None):
+def get_table_section(table_name, table, description_map=None):
     # Heading.
-    schema_string = '\n=== ' + ('`' + table_name + '` Object' if len(table_name) > 0 else 'Root') + '\n\n'
+    result = '\n=== ' + ('`' + table_name + '` Object' if len(table_name) > 0 else 'Root') + '\n\n'
 
     # Table Header.
-    schema_string += '[cols=\"1,1,1,2\", options=\"header\"]\n'
-    schema_string += '|===\n'
-    schema_string += '|Name\n'
-    schema_string += '|Type\n'
-    schema_string += '|Values\n'
-    schema_string += '|Description\n\n'
+    result += '[cols=\"1,1,1,2\", options=\"header\"]\n'
+    result += '|===\n'
+    result += '|Name\n'
+    result += '|Type\n'
+    result += '|Values\n'
+    result += '|Description\n\n'
 
     # Table Rows.
     for name, metadata in table.items():
-        schema_string += '|`' + name + '`' + (' (Optional)' if 'optional' in metadata and metadata['optional'] else '') + '\n'
-        schema_string += '|' + metadata['type'] + '\n'
-        schema_string += '|' + metadata['value'] + '\n'
+        result += '|`' + name + '`' + (' (Optional)' if 'optional' in metadata and metadata['optional'] else '') + '\n'
+        result += '|' + metadata['type'] + '\n'
+        result += '|' + (metadata['value'] if 'value' in metadata else metadata['type']) + '\n'
 
         full_name = (table_name + '.' if len(table_name) > 0 else '') + name
 
-        schema_string += '|' + (description_map[full_name] if description_map and full_name in description_map else '???') + '\n\n'
+        result += '|' + (description_map[full_name] if description_map and full_name in description_map else '???') + '\n\n'
 
     # End of Table.
-    schema_string += '|===\n'
+    result += '|===\n'
 
-    return schema_string
+    return result
 
 def get_example_section(uri, example_item, json_object):
-    output = '\n\n=== ' + example_item['name'] + '\n\n'
+    # Heading.
+    result = '\n\n=== ' + example_item['name'] + '\n\n'
 
-    output += '.GET */' + uri + ('?' + example_item['uri'] if 'uri' in example_item else '') + '* Response\n'
-    output += '[source,json5,subs="+quotes"]\n'
-    output += '----\n'
-    output += str(json_object) + '\n'
-    output += '----'
+    # Example.
+    result += '.GET */' + uri + ('?' + example_item['uri'] if 'uri' in example_item else '') + '* Response\n'
+    result += '[source,json5,subs="+quotes"]\n'
+    result += '----\n'
+    result += str(json_object) + '\n'
+    result += '----'
 
-    return output
+    return result
 
 def main():
 
@@ -272,20 +274,26 @@ def main():
         current_endpoint = endpoint_metadata['Production']
 
         # Add the documentation header.
-        output = get_header_section(current_endpoint)
+        output = get_header_section(current_endpoint, len(current_endpoint['documentation'].split('/')))
+
+        # Get a reference to the current endpoint's request details.
+        current_endpoint_request = current_endpoint['request']
 
         # Does the endpoint support any request query strings?
-        if 'query' in current_endpoint['request']:
-            output += get_request_section(current_endpoint['request']['query'])
+        if 'query' in current_endpoint_request:
+            output += get_request_section(current_endpoint_request['query'])
         
         # Perform a GET request on the resource.
-        #json_object = gateway.api_call('/' + endpoint['uri'])
+        #json_object = gateway.api_call('/' + current_endpoint_request['uri'])
         json_object = json.loads('{"production":[{"type":"inverters","activeCount":10,"readingTime":0,"wNow":0,"whLifetime":314441},{"type":"eim","activeCount":1,"measurementType":"production","readingTime":1676757919,"wNow":-0.0,"whLifetime":276144.03,"varhLeadLifetime":0.024,"varhLagLifetime":205023.785,"vahLifetime":458229.78,"rmsCurrent":0.763,"rmsVoltage":239.037,"reactPwr":175.992,"apprntPwr":182.823,"pwrFactor":0.0,"whToday":3694.0,"whLastSevenDays":49814.0,"vahToday":7451.0,"varhLeadToday":2.0,"varhLagToday":3909.0}],"consumption":[{"type":"eim","activeCount":1,"measurementType":"total-consumption","readingTime":1676757919,"wNow":370.516,"whLifetime":781493.591,"varhLeadLifetime":765078.737,"varhLagLifetime":205039.176,"vahLifetime":1254065.428,"rmsCurrent":4.567,"rmsVoltage":239.146,"reactPwr":-938.239,"apprntPwr":1092.2,"pwrFactor":0.34,"whToday":15261.591,"whLastSevenDays":101733.591,"vahToday":21683.428,"varhLeadToday":14879.737,"varhLagToday":3905.176},{"type":"eim","activeCount":1,"measurementType":"net-consumption","readingTime":1676757919,"wNow":370.516,"whLifetime":646231.428,"varhLeadLifetime":765078.713,"varhLagLifetime":15.391,"vahLifetime":1254065.428,"rmsCurrent":3.804,"rmsVoltage":239.255,"reactPwr":-762.247,"apprntPwr":908.029,"pwrFactor":0.41,"whToday":0,"whLastSevenDays":0,"vahToday":0,"varhLeadToday":0,"varhLagToday":0}],"storage":[{"type":"acb","activeCount":0,"readingTime":0,"wNow":0,"whNow":0,"state":"idle"}]}')
+
+        # Get a reference to the current endpoint's response details.
+        current_endpoint_response = current_endpoint['response']
 
         # We can override some known types.
         object_map = None
-        if 'map_object' in current_endpoint['response']:
-            object_map = current_endpoint['response']['map_object']
+        if 'map_object' in current_endpoint_response:
+            object_map = current_endpoint_response['map_object']
 
         # Get the schema recursively.
         json_schema = get_schema(json_object, object_map=object_map)
@@ -295,24 +303,24 @@ def main():
 
         # We can map some known descriptions.
         description_map = None
-        if 'map_description' in current_endpoint['response']:
-            description_map = current_endpoint['response']['map_description']
+        if 'map_description' in current_endpoint_response:
+            description_map = current_endpoint_response['map_description']
 
         for table_name, table in json_schema.items():
-            output += table_to_string(table_name, table, description_map)
+            output += get_table_section(table_name, table, description_map)
 
         # Add the examples.
         output += '\n'
         output += '== Examples'
 
         count = 1
-        for example_item in current_endpoint['request']['examples']:
+        for example_item in current_endpoint_request['examples']:
             # We skip calling the first example as this has already been queried above.
             if not test_only and count > 1:
-                json_object = gateway.api_call('/' + current_endpoint['uri'] + ('?' + example_item['uri'] if 'uri' in example_item else ''))
+                json_object = gateway.api_call('/' + current_endpoint_request['uri'] + ('?' + example_item['uri'] if 'uri' in example_item else ''))
 
             # Take the obtained JSON as an example.
-            output += get_example_section(current_endpoint['uri'], example_item, json_object)
+            output += get_example_section(current_endpoint_request['uri'], example_item, json_object)
 
         # Generate a suitable filename to store our documentation in.
         filename = 'output/' + current_endpoint['documentation']
