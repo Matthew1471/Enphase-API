b70165f2f146a682b32e5042d19585ebdba840f1
Matthew <5273302+Matthew1471@users.noreply.github.com>
2023-08-27 12:23:48 +0100

    Add Example
    - Added a Power Meter Unit (PMU) responder. The actual energy data is sent over FTP.

---
+++ Python/examples/emulate_power_meter_unit.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of Enphase-API <https://github.com/Matthew1471/Enphase-API>
+# Copyright (C) 2023 Matthew1471!
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# We output the current date/time for debugging.
+import datetime
+
+# We use IP sockets to receive and send data.
+import socket
+
+
+# Meter details.
+METER_IP_ADDRESS = '192.168.0.100'
+METER_SOFTWARE_VERSION = '1.0.0'
+METER_MAC_ADDRESS = 'aa:bb:cc:dd:ee:ff'
+
+def respond_to_PMU_probes():
+    # Create a UDP socket.
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+    # Allow port number re-use.
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # Bind to all available interfaces and port 12345.
+    sock.bind(('0.0.0.0', 12345))
+
+    # Notify the user.
+    print(str(datetime.datetime.now()) + ' - Waiting for Power Meter Unit (PMU) probes from an IQ Gateway.', flush=True)
+
+    # Repeat indefinitely.
+    while True:
+        # Wait to receive 8 bytes of data and the sender's address.
+        data, address = sock.recvfrom(8)
+
+        # Was this a Power Meter Unit (PMU) "SendData" probe?
+        if data.decode('utf-8') == 'SendData':
+            # Format the address in the conventional IP:PORT format.
+            formatted_address = str(address[0]) + ':' + str(address[1])
+
+            # Notify the user.
+            print(str(datetime.datetime.now()) + ' - Responding to a Power Meter Unit (PMU) probe from ' + formatted_address + '.', flush=True)
+
+            # Send a response back.
+            sock.sendto(bytes('IP: ' + METER_IP_ADDRESS + ' Ver: '+ METER_SOFTWARE_VERSION + ' MAC: ' + METER_MAC_ADDRESS, 'utf-8'), address)
+
+# Launch the respond_to_PMU_probes function if invoked directly.
+if __name__ == '__main__':
+    respond_to_PMU_probes()
